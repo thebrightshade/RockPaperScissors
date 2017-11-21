78aa9d54f36664808e1e42f9561a0d1c3f1780cd
Shadmaan <shadmaan@gmail.com>
2017-11-21 14:20:04 -0800

    Add files via upload

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,41 @@
+import random
+player1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
+
+player2 = random.sample({"R", "P", "S"}, 1)
+
+
+def compare(p1, p2):
+    if p1 == "S" or p1 == "SCISSORS":
+        if p2[0] == "R":
+            print("You lose! Rock beats scissors.")
+        elif p2[0] == "P":
+            print("You win! Scissors beats paper.")
+        elif p2[0] == "S":
+            print("Let's try again, you and I, both have scissors")
+            p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
+            p2 = random.sample({"R", "P", "S"}, 1)
+            compare(p1,p2)
+
+    elif p1 == "R" or p1 == "ROCK":
+        if p2[0] == "P":
+            print("You lose! Paper beats rock.")
+        elif p2[0] == "S":
+            print("You win! Rock beats scissors.")
+        elif p2[0] == "R":
+            print("Let's try again, you and I, both have rock")
+            p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
+            p2 = random.sample({"R", "P", "S"}, 1)
+            compare(p1, p2)
+
+    elif p1 == "P" or p1 == "PAPER":
+        if p2[0] == "S":
+            print("You lose! Scissors beats paper.")
+        elif p2[0] == "R":
+            print("You win! Paper beats rock.")
+        elif p2[0] == "P":
+            print("Let's try again, you and I, both have paper")
+            p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
+            p2 = random.sample({"R", "P", "S"}, 1)
+            compare(p1, p2)
+
+compare(player1, player2)
