d4e62c0e840e77fc20c66717df64b2a44330343c
TheBrightShade <shadmaan@gmail.com>
2017-11-23 00:34:27 -0800

    minor fix

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,4 +1,5 @@
 import random
+
 player1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
 
 player2 = random.sample({"R", "P", "S"}, 1)
@@ -8,43 +9,43 @@ def compare(p1, p2):
     if p1 == "S" or p1 == "SCISSORS":
         if p2[0] == "R":
             print("You lose! Rock beats scissors.")
-            replay = input("Do you want to play again (Y/N)? ").upper()
+            replay = input("Do you want to play again? (Y/N) ").upper()
             if replay == "Y":
                 p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
                 p2 = random.sample({"R", "P", "S"}, 1)
-                compare(p1,p2)
-            
+                compare(p1, p2)
+
         elif p2[0] == "P":
             print("You win! Scissors beats paper.")
-            replay = input("Do you want to play again (Y/N)? ").upper()
+            replay = input("Do you want to play again? (Y/N) ").upper()
             if replay == "Y":
                 p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
                 p2 = random.sample({"R", "P", "S"}, 1)
-                compare(p1,p2)
-                
+                compare(p1, p2)
+
         elif p2[0] == "S":
             print("Let's try again, you and I, both have scissors")
             p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
             p2 = random.sample({"R", "P", "S"}, 1)
-            compare(p1,p2)
+            compare(p1, p2)
 
     elif p1 == "R" or p1 == "ROCK":
         if p2[0] == "P":
             print("You lose! Paper beats rock.")
-            replay = input("Do you want to play again (Y/N)? ").upper()
+            replay = input("Do you want to play again? (Y/N) ").upper()
             if replay == "Y":
                 p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
                 p2 = random.sample({"R", "P", "S"}, 1)
-                compare(p1,p2)
-            
+                compare(p1, p2)
+
         elif p2[0] == "S":
             print("You win! Rock beats scissors.")
-            replay = input("Do you want to play again (Y/N)? ").upper()
+            replay = input("Do you want to play again? (Y/N) ").upper()
             if replay == "Y":
                 p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
                 p2 = random.sample({"R", "P", "S"}, 1)
-                compare(p1,p2)
-            
+                compare(p1, p2)
+
         elif p2[0] == "R":
             print("Let's try again, you and I, both have rock")
             p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
@@ -54,20 +55,20 @@ def compare(p1, p2):
     elif p1 == "P" or p1 == "PAPER":
         if p2[0] == "S":
             print("You lose! Scissors beats paper.")
-            replay = input("Do you want to play again (Y/N)? ").upper()
+            replay = input("Do you want to play again? (Y/N) ").upper()
             if replay == "Y":
                 p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
                 p2 = random.sample({"R", "P", "S"}, 1)
                 compare(p1,p2)
-            
+
         elif p2[0] == "R":
             print("You win! Paper beats rock.")
-            replay = input("Do you want to play again (Y/N)? ").upper()
+            replay = input("Do you want to play again? (Y/N) ").upper()
             if replay == "Y":
                 p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
                 p2 = random.sample({"R", "P", "S"}, 1)
-                compare(p1,p2)
-            
+                compare(p1, p2)
+
         elif p2[0] == "P":
             print("Let's try again, you and I, both have paper")
             p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
@@ -75,7 +76,8 @@ def compare(p1, p2):
             compare(p1, p2)
     else:
         print('You did not type a correct response!')
-        player1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
-        compare(player1, player2)
+        p1 = (input("Please select one: Rock(R), Paper(P) or Scissors(S): ")).upper()
+        compare(p1, p2)
+
 
 compare(player1, player2)
